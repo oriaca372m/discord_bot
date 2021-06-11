3b6215711a5d8f5267eb0d8cb80906e70ce88b9c
oriaca <oriaca372m@outlook.jp>
2021-06-12 00:58:06 +0900

    リファクタリング

--- src/config/feature-loader.ts
+++ src/config/feature-loader.ts
@@ -19,115 +19,77 @@ export const FeatureConfigBase = t.intersection([
 ])
 export type FeatureConfigBaseType = t.TypeOf<typeof FeatureConfigBase>
 
-function loadFeatureSimpleReply(_entry: FeatureConfigBaseType): FeatureSimpleReply {
-	return new FeatureSimpleReply()
-}
+type Loader = (entry: FeatureConfigBaseType) => FeatureInterface
 
-function loadFeatureMondai(entry: FeatureConfigBaseType): FeatureMondai {
-	const Config = t.type({
-		command_name: t.string,
-		config_path: t.string,
-	})
-	const result = Config.decode(entry)
-	if (isLeft(result)) {
-		throw PathReporter.report(result)
+function makeLoader<T extends t.Mixed>(
+	ConfigType: T,
+	converter: (entry: t.TypeOf<T>) => FeatureInterface
+): Loader {
+	return (entry: FeatureConfigBaseType) => {
+		const result = ConfigType.decode(entry)
+		if (isLeft(result)) {
+			throw PathReporter.report(result)
+		}
+		return converter(result.right)
 	}
-
-	const cfg = result.right
-	return new FeatureMondai(cfg.command_name, cfg.config_path)
 }
 
-function loadFeatureCustomReply(entry: FeatureConfigBaseType): FeatureCustomReply {
-	const Config = t.type({
-		command_name: t.string,
-	})
-	const result = Config.decode(entry)
-	if (isLeft(result)) {
-		throw PathReporter.report(result)
-	}
+const loaders: { [key: string]: Loader } = {
+	simple_reply: () => new FeatureSimpleReply(),
 
-	const cfg = result.right
-	return new FeatureCustomReply(cfg.command_name)
-}
+	mondai: makeLoader(
+		t.type({
+			command_name: t.string,
+			config_path: t.string,
+		}),
+		(cfg) => new FeatureMondai(cfg.command_name, cfg.config_path)
+	),
 
-function loadFeatureCommandAlias(entry: FeatureConfigBaseType): FeatureCommandAlias {
-	const Config = t.type({
-		command_name: t.string,
-		to_name: t.string,
-		to_args: t.array(t.string),
-	})
-	const result = Config.decode(entry)
-	if (isLeft(result)) {
-		throw PathReporter.report(result)
-	}
+	custom_reply: makeLoader(
+		t.type({
+			command_name: t.string,
+		}),
+		(cfg) => new FeatureCustomReply(cfg.command_name)
+	),
 
-	const cfg = result.right
-	return new FeatureCommandAlias(cfg.command_name, cfg.to_name, cfg.to_args)
-}
+	command_alias: makeLoader(
+		t.type({
+			command_name: t.string,
+			to_name: t.string,
+			to_args: t.array(t.string),
+		}),
+		(cfg) => new FeatureCommandAlias(cfg.command_name, cfg.to_name, cfg.to_args)
+	),
 
-function loadFeaturePlayMusic(entry: FeatureConfigBaseType): FeaturePlayMusic {
-	const Config = t.type({
-		command_name: t.string,
-	})
-	const result = Config.decode(entry)
-	if (isLeft(result)) {
-		throw PathReporter.report(result)
-	}
+	play_music: makeLoader(
+		t.type({
+			command_name: t.string,
+		}),
+		(cfg) => new FeaturePlayMusic(cfg.command_name)
+	),
 
-	const cfg = result.right
-	return new FeaturePlayMusic(cfg.command_name)
-}
+	sk: makeLoader(
+		t.type({
+			sk_command_name: t.string,
+			set_command_name: t.string,
+		}),
+		(cfg) => new FeatureSk(cfg.sk_command_name, cfg.set_command_name)
+	),
 
-function loadFeatureSk(entry: FeatureConfigBaseType): FeatureSk {
-	const Config = t.type({
-		sk_command_name: t.string,
-		set_command_name: t.string,
-	})
-	const result = Config.decode(entry)
-	if (isLeft(result)) {
-		throw PathReporter.report(result)
-	}
+	web_api: makeLoader(
+		t.type({
+			port: t.number,
+		}),
+		(cfg) => new FeatureWebApi(cfg.port)
+	),
 
-	const cfg = result.right
-	return new FeatureSk(cfg.sk_command_name, cfg.set_command_name)
-}
-
-function loadFeatureWebApi(entry: FeatureConfigBaseType): FeatureWebApi {
-	const Config = t.type({
-		port: t.number,
-	})
-	const result = Config.decode(entry)
-	if (isLeft(result)) {
-		throw PathReporter.report(result)
-	}
-
-	const cfg = result.right
-	return new FeatureWebApi(cfg.port)
-}
-
-function loadFeatureBasicWebApiMethods(entry: FeatureConfigBaseType): FeatureBasicWebApiMethods {
-	const Config = t.type({
-		webui_command_name: t.string,
-		webui_url: t.string,
-	})
-	const result = Config.decode(entry)
-	if (isLeft(result)) {
-		throw PathReporter.report(result)
-	}
-
-	const cfg = result.right
-	return new FeatureBasicWebApiMethods(cfg.webui_command_name, cfg.webui_url)
-}
-
-const loaders: { [key: string]: (entry: FeatureConfigBaseType) => FeatureInterface } = {
-	mondai: loadFeatureMondai,
-	simple_reply: loadFeatureSimpleReply,
-	custom_reply: loadFeatureCustomReply,
-	command_alias: loadFeatureCommandAlias,
-	play_music: loadFeaturePlayMusic,
-	sk: loadFeatureSk,
-	web_api: loadFeatureWebApi,
-	basic_web_api_methods: loadFeatureBasicWebApiMethods,
+	basic_web_api_methods: makeLoader(
+		t.type({
+			webui_command_name: t.string,
+			webui_url: t.string,
+		}),
+		(cfg) => new FeatureBasicWebApiMethods(cfg.webui_command_name, cfg.webui_url)
+	),
 }
 
 export class FeatureLoader {
