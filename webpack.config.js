43d2b3f765a694ddef98407c27fd7046b5c22f83
oriaca <oriaca372m@outlook.jp>
2021-06-13 00:08:35 +0900

    eslint-loaderを削除

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,11 @@ const NodemonPlugin = require('nodemon-webpack-plugin')
 module.exports = {
 	...common,
 	mode: 'development',
-	module: {
-		...common.module,
-		rules: [
-			{
-				enforce: 'pre',
-				test: /\.(ts|js)$/,
-				exclude: /node_modules/,
-				loader: 'eslint-loader',
-				options: {
-					fix: true
-				}
-			},
-			...common.module.rules
-		]
-	},
 	cache: {
 		type: 'filesystem',
 		buildDependencies: {
-			config: ['./webpack.common.js', './webpack.config.js']
-		}
+			config: ['./webpack.common.js', './webpack.config.js'],
+		},
 	},
-	plugins: [
-		new NodemonPlugin({
-			nodeArgs: ['-r', 'dotenv/config']
-		})
-	]
+	plugins: [new NodemonPlugin()],
 }
