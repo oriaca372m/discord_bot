df46a3a77dac12c523183ed4d5f672b50be296ab
oriaca <oriaca372m@outlook.jp>
2024-01-14 23:19:17 +0900

    typescript-eslintの設定を更新

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,23 +5,22 @@ module.exports = {
 	},
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
 		'plugin:jest/recommended',
 		'plugin:jest/style',
 		'prettier',
 	],
-	plugins: ['jest'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'jest'],
+	root: true,
 	globals: {
 		Atomics: 'readonly',
 		SharedArrayBuffer: 'readonly',
 	},
-	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		project: './tsconfig.json',
 	},
 	rules: {
 		'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-		'@typescript-eslint/no-empty-interface': 'off',
 	},
 }
